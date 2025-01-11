ee5da51ce8945e9b758b926931d559f4780c9d3c
Caio Vieira <caioviier@gmail.com>
2025-01-10 22:08:06 -0300

    feat: hello world

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const express = require('./express');
+const express = require('./express')
+const routes = require('./server/routes')
 
 const port = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(routes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
