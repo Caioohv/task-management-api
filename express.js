a25cad7653644ce4c201c2fc7eb3c4ad52653e9c
Caio Vieira <caioviier@gmail.com>
2025-01-10 21:15:38 -0300

    feat: server setup

---
+++ express.js
@@ -0,0 +1,33 @@
+module.exports = () => {
+  const express = require('express');
+  const morgan = require('morgan')
+  const cors = require('cors')
+  
+  require('dotenv').config();
+  
+  const port = process.env.PORT || 3000;
+  const app = express();
+  
+  morgan.token('statusColor', (req, res, args) => {
+    var status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent)
+      ? res.statusCode
+      : undefined
+  
+    var color = status >= 500 ? 31 // red
+      : status >= 400 ? 33 // yellow
+        : status >= 300 ? 36 // cyan
+          : status >= 200 ? 32 // green
+            : 0 // no color
+  
+    return '\x1b[' + color + 'm' + status + '\x1b[0m'
+  })
+  
+  app.use(morgan('\x1b[33m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor :response-time ms'))
+  app.use(cors())
+
+  return app;
+}
+
+
+
+
